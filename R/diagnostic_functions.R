a36e4cc4e2f0bf4884595268b4203a4c485af6ea
Hao Ye <hye@ucsd.edu>
2015-10-05 18:02:05 -0700

    - updates to the documentation (with much thanks and credit to Adam Clark and Ethan Deyle) - added nonlinearity test function - minor tweaks to s-map function (default E)

--- R/diagnostic_functions.R
+++ R/diagnostic_functions.R
@@ -1,6 +1,55 @@
+#' Randomization test for nonlinearity using S-maps and surrogate data
+#' 
+#' \code{test_nonlinearity} tests for nonlinearity using S-maps by 
+#' comparing improvements in forecast skill (delta rho and delta mae) between 
+#' linear and nonlinear models with a null distribution from surrogate data.
+#' 
+#' @param ts the original time series
+#' @param method which algorithm to use to generate surrogate data
+#' @param num_surr the number of null surrogates to generate
+#' @param T_period the period of seasonality for seasonal surrogates (ignored for other methods)
+#' @param ... optional arguments to s_map
+#' @return A list containing the following components:
+#' \tabular{ll}{
+#'   delta_rho \tab the value of the delta rho statistic\cr
+#'   delta_mae \tab the value of the delat mae statistic\cr
+#'   num_surr \tab the size of the null distribution\cr
+#'   delta_rho_p_value \tab the p-value for delta rho\cr
+#'   delta_mae_p_value \tab the p-value for delta mae\cr
+#' }
+#' @export 
+#' 
+
+test_nonlinearity <- function(ts, method = "ebisuzaki", num_surr = 200, T_period = 1, E = 1, ...)
+{
+    compute_stats <- function(ts, ...)
+    {
+        results <- s_map(ts, stats_only = TRUE, silent = TRUE, ...)
+        delta_rho <- max(results$rho) - results$rho[results$theta == 0]
+        delta_mae <- results$mae[results$theta == 0] - min(results$mae)
+        return(c(delta_rho = delta_rho, delta_mae = delta_mae))
+    }
+    
+    actual_stats <- compute_stats(ts, ...)
+    delta_rho <- actual_stats["delta_rho"]
+    delta_mae <- actual_stats["delta_mae"]
+    names(delta_rho) <- NULL
+    names(delta_mae) <- NULL
+    surrogate_data <- make_surrogate_data(ts, method, num_surr, T_period)
+    null_stats <- data.frame(t(apply(surrogate_data, 2, compute_stats, ...)))
+    
+    return(list(delta_rho = delta_rho, 
+                delta_mae = delta_mae, 
+                num_surr = num_surr, 
+                E = E, 
+                delta_rho_p_value = (sum(null_stats$delta_rho > delta_rho)+1) / (num_surr+1), 
+                delta_mae_p_value = (sum(null_stats$delta_mae > delta_mae)+1) / (num_surr+1)))
+}
+
+
 #' Generate surrogate data for permutation/randomization tests
 #'
-#' \code{make_surrogate data} generates surrogate data under several different 
+#' \code{make_surrogate_data} generates surrogate data under several different 
 #' null models.
 #' 
 #' Method "random_shuffle" creates surrogates by randomly permuting the values 
@@ -8,6 +57,9 @@
 #' 
 #' Method "Ebisuzaki" creates surrogates by randomizing the phases of a Fourier 
 #' transform, preserving the power spectra of the null surrogates.
+#' 
+#' Method "seasonal" creates surrogates by computing a mean seasonal trend of 
+#' the specified period and shuffling the residuals.
 #' 
 #' See \code{test_nonlinearity} for context.
 #' 
