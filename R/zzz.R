6a77ca9e4bf1fd7da1f4ec29b0391c79b6db0765
Hao Ye <hye@ucsd.edu>
2017-06-15 17:57:35 -0700

    updated vignette (rEDM-tutorial) up to real-data examples

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ loadModule("xmap_module", TRUE)
     if (!interactive()) return()
     
     intro_message <- paste("If you're new to the rEDM package, please check out the tutorial:",
-                           "> vignette(\"rEDM_tutorial\")", sep = "\n")
+                           "> vignette(\"rEDM-tutorial\")", sep = "\n")
     packageStartupMessage(intro_message)
 }
