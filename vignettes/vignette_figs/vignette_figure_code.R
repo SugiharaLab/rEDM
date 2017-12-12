151c92c2c1781865348c79df5aca026fef37cc8e
Hao Ye <lhopitalified@gmail.com>
2017-12-12 17:44:52 -0500

    updating vignette to include revisions through e120 example and R code to produce figures 1 and 3

--- vignettes/vignette_figs/vignette_figure_code.R
+++ vignettes/vignette_figs/vignette_figure_code.R
@@ -53,39 +53,43 @@ rescale <- function(x, buffer = 0.15, ...)
     return((x - min(x, ...)) / (max(x, ...) - min(x, ...)) * (1 - 2 * buffer) + buffer)
 }
 
-make_attractor_plot <- function(x, y, z, 
-                                t = floor(length(x) / 2), 
-                                rescale_buffer = 0.05, 
-                                userMatrix = matrix(c(0.0001, 0.2484, 0.9687, 0.05, 
-                                                      0.9417, -0.3258, 0.0835, 0, 
-                                                      0.3367, 0.9122, -0.2339, 0, 
-                                                      0, 0, 0, 1), nrow = 4, byrow = TRUE),
-                                FOV = 15, zoom = 0.7, 
-                                scale = c(1, 1, 1), 
-                                windowRect = c(50, 50, 1000, 750),
-                                plot_file = NULL)
+# Plot Lorenz attractor with projected time series
+make_figure_1 <- function(dat, t = 1120, future_len = 40, 
+                          rescale_buffer = 0.05, 
+                          userMatrix = matrix(c(0.0001, 0.2484, 0.9687, 0.05, 
+                                                0.9417, -0.3258, 0.0835, 0, 
+                                                0.3367, 0.9122, -0.2339, 0, 
+                                                0, 0, 0, 1), nrow = 4, byrow = TRUE),
+                          FOV = 15, zoom = 0.7, cex = 1.75, scale = c(1, 1, 1), 
+                          windowRect = c(50, 50, 700, 500),
+                          plot_file = NULL)
 {
-    x <- rescale(x, rescale_buffer)
-    y <- rescale(-y, rescale_buffer)
-    z <- rescale(z, rescale_buffer)
+    par3d(windowRect = windowRect, scale = scale, 
+          userMatrix = userMatrix, 
+          zoom = zoom, FOV = FOV, cex = cex)
+    
+    idx <- seq(from = 1, to = t + future_len)
+    x <- rescale(dat[idx, 1], rescale_buffer)
+    y <- rescale(-dat[idx, 2], rescale_buffer)
+    z <- rescale(dat[idx, 3], rescale_buffer)
     
     past_t <- seq(from = 1, to = t)
-    future_t <- seq(from = t, to = length(x))
+    future_t <- seq(from = t, length.out = future_len)
     if (length(future_t) %% 2 == 1)
         future_t <- future_t[-1]
     plot3d(x[past_t], y[past_t], z[past_t], type = "l", lwd = 2, 
            xlab = "", ylab = "", zlab = "", axes = FALSE)
     segments3d(x[future_t], y[future_t], z[future_t], lwd = 1)
-
+    
     # draw axes
     lines3d(c(0, 1), 0, 0, lwd = 2)
-    text3d(0.75, 0, -0.05, "x", cex = 1.5, 
+    text3d(0.75, 0, -0.05, "x", 
            family = "serif", font = 3, usePlotmath = FALSE)
     lines3d(0, c(0, 1), 0, lwd = 2)
-    text3d(0, 0.5, -0.1, "y", cex = 1.5, 
+    text3d(0, 0.5, -0.1, "y", 
            family = "serif", font = 3, usePlotmath = FALSE)
     lines3d(0, 1, c(0, 1), lwd = 2)
-    text3d(0, 1.15, 0.6, "z", cex = 1.5, 
+    text3d(0, 1.15, 0.6, "z", 
            family = "serif", font = 3, usePlotmath = FALSE)
     
     # draw projected time series
@@ -95,18 +99,8 @@ make_attractor_plot <- function(x, y, z,
             ts_path * cos(theta_x), 
             ts_path * sin(theta_x), 
             color = color_red, lwd = 2)
-    pos_text <- 1
-    text3d(-0.05, pos_text * cos(theta_x), pos_text * sin(theta_x), "time", 
-           cex = 1.5)
-    arrow_start <- 0.85
-    arrow_end <- 0.5
-    lines3d(x = c(-0.05, -0.05), 
-            y = c(arrow_start * cos(theta_x), arrow_end * cos(theta_x)), 
-            z = c(arrow_start * sin(theta_x), arrow_end * sin(theta_x)), 
-            lwd = 2)
-    # arrow3d(c(-0.05, arrow_start * cos(theta_x), -arrow_start * sin(theta_x)), 
-    #         c(-0.05, arrow_end * cos(theta_x), -arrow_end * sin(theta_x)), 
-    #         type = "flat", s = 0.15, theta = pi/8)
+    pos_text <- 1.2
+    text3d(-0.05, pos_text * cos(theta_x), pos_text * sin(theta_x), "time")
     
     # draw projection from attractor to axis
     xx <- x[t]
@@ -114,29 +108,97 @@ make_attractor_plot <- function(x, y, z,
     zz <- z[t]
     points3d(xx, yy, zz, color = "black", size = 8)
     lines3d(c(xx, xx, xx), c(0, yy, yy), c(0, 0, zz), color = color_red, lwd = 1.5)
-
+    
     par3d(windowRect = windowRect, scale = scale, 
-        userMatrix = userMatrix, 
-        zoom = zoom, FOV = FOV)
-
+          userMatrix = userMatrix, 
+          zoom = zoom, FOV = FOV, cex = cex)
+    
     if (!is.null(plot_file))
         rgl.postscript(plot_file, fmt = "pdf")
     
     return()
 }
 
-fig_1_file <- here("vignettes", "vignette_figs", "figure_1.pdf")
-dat <- generate_lorenz_attractor()
-make_attractor_plot(dat[1:1160, 1],
-                    dat[1:1160, 2],
-                    dat[1:1160, 3], 
-                    t = 1120, 
-                    plot_file = fig_1_file)
+# Plot reconstructed attractors using lags of x and y
+make_figure_3 <- function(dat, t = 1220, 
+                          shift = 0.55, 
+                          rescale_buffer = 0.05, 
+                          userMatrix = matrix(c(0.9809, 0.1941, -0.0105, 0, 
+                                                -0.0382, 0.2453, 0.9687, 0, 
+                                                0.1906, -0.9498, 0.2481, 0, 
+                                                0, 0, 0, 1), nrow = 4, byrow = TRUE),
+                          FOV = 10, zoom = 0.7, cex = 1.75, scale = c(1, 1, 1), 
+                          windowRect = c(50, 50, 700, 500),
+                          plot_file = NULL)
+{
+    
+    draw_lagged_attractor <- function(v, FUN = lines3d,
+                                      idx = seq(from = 1, to = t), tau = 8, 
+                                      spacing = c(0, 0, 0),  ...)
+    {
+        FUN <- match.fun(FUN)
+        xx <- v[idx + 2 * tau]
+        yy <- v[idx + tau]
+        zz <- v[idx]
+        
+        FUN(xx + spacing[1], 
+            yy + spacing[2], 
+            zz + spacing[3], ...)
+        
+        return(c(tail(xx, 1) + spacing[1], 
+                 tail(yy, 1) + spacing[2], 
+                 tail(zz, 1) + spacing[3]))
+    }
+    
+    par3d(windowRect = windowRect, scale = scale, 
+          #  userMatrix = userMatrix, 
+          zoom = zoom, FOV = FOV, cex = cex)
+    
+    x <- rescale(dat[, 1], buffer = rescale_buffer)
+    y <- rescale(dat[, 2], buffer = rescale_buffer)
+    
+    plot3d(0, 0, 0, type = "n", 
+           xlab = "", ylab = "", zlab = "", axes = FALSE)
+    
+    spacing_x <- c(-shift, 0, 0)
+    m_x_t <- draw_lagged_attractor(x, spacing = spacing_x, lwd = 2)
+    points3d(m_x_t[1], m_x_t[2], m_x_t[3], color = "black", size = 8)
+    draw_lagged_attractor(x, idx = seq(from = t + 1, length.out = 80), 
+                          spacing = spacing_x, 
+                          FUN = segments3d, lwd = 1)
+    text3d(0.2 + spacing_x[1], 0.5 + spacing_x[2], 1 + spacing_x[3], "M_x", cex = 2)
+    
+    spacing_y <- c(shift, 0, 0)
+    m_y_t <- draw_lagged_attractor(y, spacing = spacing_y, lwd = 2)
+    points3d(m_y_t[1], m_y_t[2], m_y_t[3], color = "black", size = 8)
+    draw_lagged_attractor(y, idx = seq(from = t + 1, length.out = 80), 
+                          spacing = spacing_y, 
+                          FUN = segments3d, lwd = 1)
+    text3d(1 + spacing_y[1], 0.5 + spacing_y[2], 1 + spacing_y[3], "M_y", cex = 2)
+    
+    lines3d(rbind(m_x_t, m_y_t), color = color_blue)
+    
+    if (!is.null(plot_file))
+        rgl.postscript(plot_file, fmt = "pdf")
+    
+    return()
+}
 
-# img <- image_read(fig_1_file)
-# print(img)
-# image_trim(img)
-# print(img)
-# image_write(img, path = fig_1_file)
+
+dat <- generate_lorenz_attractor()
+
+if(FALSE)
+{
+    fig_1_file <- here("vignettes", "vignette_figs", "figure_1.pdf")
+    make_figure_1(dat, plot_file = fig_1_file)
+}
+
+fig_2_file <- here("vignettes", "vignette_figs", "figure_2.pdf")
+
+if(TRUE)
+{
+    fig_3_file <- here("vignettes", "vignette_figs", "figure_3.pdf")
+    make_figure_3(dat, plot_file = fig_3_file)
+}
 
 
