129ef37253ab6502d4956960a552f44cc97d0a85
Hao Ye <lhopitalified@gmail.com>
2018-01-29 16:02:21 -0500

    updating documentation for datasets and cleaning unused datasets

--- R/rEDM_package.R
+++ R/rEDM_package.R
@@ -42,87 +42,136 @@
 #' @keywords package
 NULL
 
-#' @name paramecium_didinium
-#' @docType data
 #' @title Time series for the Paramecium-Didinium laboratory experiment
 #' @author Veilleux
 #' @description Time series of Paramecium and Didinium abundances (#/mL) from 
-#' an experiment by Veilleux (1979)
-NULL
+#'   an experiment by Veilleux (1979)
+#' @format
+#' \describe{
+#'   \item{\code{time}}{time of sampling (in days)}
+#'   \item{\code{paramecium}}{paramecium abundance (per mL)}
+#'   \item{\code{didinium}}{didinium abundance (per mL)}
+#' }
+"paramecium_didinium"
 
-#' @name sardine_anchovy_sst
-#' @docType data
 #' @title Time series for the California Current Anchovy-Sardine-SST system
 #' @author ****
 #' @description Time series of Pacific sardine landings (CA), Northern anchovy 
 #'   landings (CA), and sea-surface temperature (3-year average) at the SIO 
 #'   pier and Newport pier
-NULL
+#' @format
+#' \describe{
+#'   \item{\code{year}}{year of measurement}
+#'   \item{\code{anchovy}}{anchovy landings, scaled to mean = 0, sd = 1}
+#'   \item{\code{sardine}}{sardine landings, scaled to mean = 0, sd = 1}
+#'   \item{\code{sio_sst}}{3-year running average of sea surface temperature at 
+#'     SIO pier, scaled to mean = 0, sd = 1}
+#'   \item{\code{np_sst}}{3-year running average of sea surface temperature at 
+#'     Newport pier, scaled to mean = 0, sd = 1}
+#' }
+"sardine_anchovy_sst"
 
-#' @name tentmap_del
-#' @docType data
 #' @title Time series for a tent map with mu = 2.
 #' @author Hao Ye
-#' @description First-differenced time series generated from the tent map 
+#' @description First-differenced time series generated from the tent map
 #'   recurrence relation with mu = 2.
-NULL
+#' @format A numeric vector with 999 values
+"tentmap_del"
 
-#' @name two_species_model
-#' @docType data
 #' @title Time series for a two-species coupled model.
 #' @author Hao Ye
 #' @description Time series generated from a discrete-time coupled 
 #'   Lotka-Volterra model exhibiting chaotic dynamics.
-NULL
+#' @format A data frame with 1000 rows and 3 variables: 
+#' \describe{
+#'   \item{\code{time}}{time index (# of generations)}
+#'   \item{\code{x}}{abundance of simulated species x}
+#'   \item{\code{y}}{abundance of simulated species y}
+#' }
+"two_species_model"
 
-#' @name block_3sp
-#' @docType data
 #' @title Time series for a three-species coupled model.
 #' @author Hao Ye
 #' @description Time series generated from a discrete-time coupled 
 #'   Lotka-Volterra model exhibiting chaotic dynamics.
-NULL
+#' @format A data frame with 200 rows and 10 variables: 
+#' \describe{
+#'   \item{\code{time}}{time index (# of generations)}
+#'   \item{\code{x_t}}{abundance of simulated species $x$ at time $t$}
+#'   \item{\code{x_t-1}}{abundance of simulated species $x$ at time $t-1$}
+#'   \item{\code{x_t-2}}{abundance of simulated species $x$ at time $t-2$}
+#'   \item{\code{y_t}}{abundance of simulated species $y$ at time $t$}
+#'   \item{\code{y_t-1}}{abundance of simulated species $y$ at time $t-1$}
+#'   \item{\code{y_t-2}}{abundance of simulated species $y$ at time $t-2$}
+#'   \item{\code{z_t}}{abundance of simulated species $z$ at time $t$}
+#'   \item{\code{z_t-1}}{abundance of simulated species $z$ at time $t-1$}
+#'   \item{\code{z_t-2}}{abundance of simulated species $z$ at time $t-2$}
+#' }
+"block_3sp"
 
-#' @name sockeye_returns
-#' @docType data
 #' @title Time series for sockeye salmon returns.
-#' @author ****
+#' @author Fisheries and Oceans Canada
 #' @description Time series of sockeye salmon returns from the Fraser River in 
-#' British Columbia, Canada.
-NULL
+#'   British Columbia, Canada.
+#' @format A data frame with 55 rows and 10 variables: 
+#' \describe{
+#'   \item{\code{year}}{year}
+#'   \item{\code{Early_Stuart}}{number of sockeye returns for the Early Stuart 
+#'     stock (millions of fish)}
+#'   \item{\code{Late_Stuart}}{number of sockeye returns for the Late Stuart 
+#'     stock (millions of fish)}
+#'   \item{\code{Stellako}}{number of sockeye returns for the Stellako 
+#'     stock (millions of fish)}
+#'   \item{\code{Quesnel}}{number of sockeye returns for the Quesnel 
+#'     stock (millions of fish)}
+#'   \item{\code{Chilko}}{number of sockeye returns for the Chilko
+#'     stock (millions of fish)}
+#'   \item{\code{Seymour}}{number of sockeye returns for the Seymour
+#'     stock (millions of fish)}
+#'   \item{\code{Late_Shuswap}}{number of sockeye returns for the Late Shuswap
+#'     stock (millions of fish)}
+#'   \item{\code{Birkenhead}}{number of sockeye returns for the Birkenhead
+#'     stock (millions of fish)}
+#'   \item{\code{Weaver}}{number of sockeye returns for the Weaver
+#'     stock (millions of fish)}
+#' }
+"sockeye_returns"
 
-#' @name e054_succession
-#' @docType data
-#' @title Succession data at the Cedar Creek LTER
-#' @author ****
-#' @description Experiment 054 is a subset of the long-term observational study 
-#' of old field succession at the Cedar Creek LTER.
-NULL
-
-#' @name e120_biodiversity
-#' @docType data
-#' @title Biodiversity data at the Cedar Creek LTER
-#' @author ****
-#' @description Experiment 120, the "Big Biodiversity" experiment at Cedar 
-#'   Creek LTER. This experiment is the longest running randomized test for the 
-#'   effects of plant diversity on ecosystem functions.
-NULL
-
-#' @name thrips_block
-#' @docType data
 #' @title Apple-blossom Thrips time series
 #' @author ****
 #' @description Seasonal outbreaks of Thrips imaginis.
-NULL
+#' @format A data frame with 81 rows and 6 variables: 
+#' \describe{
+#'   \item{\code{Year}}{year}
+#'   \item{\code{Month}}{month}
+#'   \item{\code{Thrips_imaginis}}{counts of Thrips imaginis}
+#'   \item{\code{maxT_degC}}{mean maximum dail temperature in each month 
+#'     (Celsius)}
+#'   \item{\code{Rain_mm}}{monthly rainfall (mm)}
+#'   \item{\code{Season}}{annual sinusoidal that peaks in December}
+#' }
+"thrips_block"
 
-#' @name e120_invnit16
-#' @docType data
 #' @title Biodiversity data at the Cedar Creek LTER
 #' @author ****
 #' @description Experiment 120, the "Big Biodiversity" experiment at Cedar 
 #'   Creek LTER. This experiment is the longest running randomized test for the 
-#'   effects of plant diversity on ecosystem functions.
-NULL
+#'   effects of plant diversity on ecosystem functions. This is a subset of the 
+#'   data from the treatments with 16 planted species.
+#' @format A data frame with 238 rows and 7 variables: 
+#' \describe{
+#'   \item{\code{Exp}}{experiment code (E120)}
+#'   \item{\code{Year}}{year}
+#'   \item{\code{Plot}}{plot identity}
+#'   \item{\code{AbvBioAnnProd}}{annual above-ground productivity of planted 
+#'     species (g/m^2)}
+#'   \item{\code{noh020tot}}{soil nitrate levels in the top 20cm of soil (ug/kg 
+#'     soil)}
+#'   \item{\code{invrichness}}{species richness of unplanted species in the 
+#'     plot, i.e. weeds}
+#'   \item{\code{SummerPrecip.mm}}{annual precipitation from May to August (mm)}
+#' }
+"e120_invnit16"
 
 #' @name compute_stats
 #' 
