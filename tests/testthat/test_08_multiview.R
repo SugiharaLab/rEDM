38527c6a570344a14ff869c050ed4971f8fd57ca
Hao Ye <lhopitalified@gmail.com>
2018-01-30 17:37:18 -0500

    fixing docs for make_block and tests for multiview

--- tests/testthat/test_08_multiview.R
+++ tests/testthat/test_08_multiview.R
@@ -18,8 +18,8 @@ test_that("multiview works", {
     expect_true("mae" %in% names(output))
     expect_true("rmse" %in% names(output))
     expect_equal(NROW(output), 3)
-    expect_equal(digest::digest(output),
-                 "1ad6196847edf582c7e72bbce48cd28f")
+    expect_equal(digest::digest(round(output$rho, 4)),
+                 "605e8e3ede358655d719b453223f9c9a")
 })
 
 test_that("multiview model_output works", {
@@ -30,13 +30,13 @@ test_that("multiview model_output works", {
     expect_s3_class(output, "data.frame")
     expect_true("model_output" %in% names(output))
     expect_true(is.list(output$model_output))
-    expect_error(model_output <- output$model_output[[1]], NA)
+    expect_error(model_output <- output$model_output[[3]], NA)
     expect_s3_class(model_output, "data.frame")
     expect_true("time" %in% names(model_output))
     expect_true("obs" %in% names(model_output))
     expect_true("pred" %in% names(model_output))
     expect_true("pred_var" %in% names(model_output))
     expect_equal(dim(model_output), c(99, 4))
-    expect_equal(digest::digest(output),
-                 "985468fbb9326ec2da849568c7e98504")
+    expect_equal(digest::digest(round(model_output, 4)),
+                 "52765031788900838bc0de54a3734373")
 })
